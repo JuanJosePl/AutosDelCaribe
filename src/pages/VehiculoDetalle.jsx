8b016ee42af5f303addfff0efc575bf419a7c1b2
Juan Jose Polo Lobelo <poloj3614@gmail.com>
2025-10-25 02:56:17 -0500

    fix: Corregir import de CarCardx

--- src/pages/VehiculoDetalle.jsx
+++ src/pages/VehiculoDetalle.jsx
@@ -7,7 +7,7 @@ import {
   FaShareAlt, FaHeart, FaPhone, FaChevronLeft, FaChevronRight
 } from 'react-icons/fa';
 import autosData from '../data/autos.json';
-import CarCard from './components/CarCard';
+import CarCard from '../components/CarCard';
 const VehiculoDetalle = () => {
   const { id } = useParams();
   const vehiculo = autosData.find(car => car.id === parseInt(id));
